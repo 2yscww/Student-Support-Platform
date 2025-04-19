3159268b039d624a802be490280d74c2ebbae7c2
l1mit <BrokenRing31@gmail.com>
2025-04-20 02:55:31 +0800

    [ADD] Initialize Spring Boot framework for backend

---
+++ SupportPlatformBackend/platfrom/src/main/java/team/work/platfrom/PlatfromApplication.java
@@ -0,0 +1,13 @@
+package team.work.platfrom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatfromApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlatfromApplication.class, args);
+	}
+
+}
---
+++ SupportPlatformBackend/platfrom/src/test/java/team/work/platfrom/PlatfromApplicationTests.java
@@ -0,0 +1,13 @@
+package team.work.platfrom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatfromApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
