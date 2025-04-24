ac917e50e445a04f371d5df2fae18076773550f1
l1mit <BrokenRing31@gmail.com>
2025-04-25 02:26:21 +0800

    [ADD] Add table backup sql file

--- DOC/table_backup.sql
+++ DOC/table_backup.sql
@@ -6,7 +6,7 @@
 
 CREATE TABLE users (
     user_id BIGINT PRIMARY KEY AUTO_INCREMENT,
-    username VARCHAR(50) NOT NULL,
+    username VARCHAR(50) NOT NULL UNIQUE,
     email VARCHAR(100) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
     credit_score INT DEFAULT 100,
